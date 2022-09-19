989cad8daf5c34e3418b4670beb5d9124416b10e
Omprakash Ahirwar <omprakash.ahirwar@ba.com>
2022-09-19 15:09:07 +0530

    updatd files

---
+++ src/main/java/actionsdemo/dct/DCTApplication.java
@@ -0,0 +1,16 @@
+package actionsdemo.dct;
+
+public class DCTApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		if (args.length<2)
+			System.out.print("Please share two files path for data comparision");
+		
+		System.out.print("First file: " + args[0]);
+		System.out.print("First file: " + args[1]);
+
+	}
+
+}
